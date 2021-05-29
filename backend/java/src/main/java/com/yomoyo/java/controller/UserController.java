56a3323f891a27cdf41f16a45798ced5a4da80a1
ArikoVladislav <v.ariko@bk.ru>
2021-05-29 13:14:48 +0300

    Crossorigin add.

--- backend/java/src/main/java/com/yomoyo/java/controller/UserController.java
+++ backend/java/src/main/java/com/yomoyo/java/controller/UserController.java
@@ -17,19 +17,23 @@ public final UserServices userServices;
         this.userServices = userServices;
     }
     @GetMapping(path = "/all" )
+    @CrossOrigin("http://localhost:8080")
     public List<User> findUsers()
     {
         return userServices.findAllUsers();
     }
     @PostMapping(path = "/new")
+    @CrossOrigin("http://localhost:8080")
     public void newUser(@RequestBody User user ){
          userServices.saveUser(user);
     }
     @PutMapping(path = "/update")
+    @CrossOrigin("http://localhost:8080")
     public void updateUser(@RequestBody User user){
         userServices.updateUser(user.getIduser(),user.getName(),user.getSurname(),user.getLogin(),user.getPassword());
     }
     @DeleteMapping(path = "/deletebyid")
+    @CrossOrigin("http://localhost:8080")
     public void deleteUserById(@RequestParam Long iduser ){
         userServices.deleteUser(iduser);
     }

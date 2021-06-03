0bbd4770220169a36a42f84d0045c7f45049dab2
ArikoVladislav <v.ariko@bk.ru>
2021-06-03 17:38:05 +0300

    output and add publication.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Publication msg="Welcome to Your Vue.js App"/>
   </div>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Publication from './components/Publication.vue'
 export default {
   name: 'App',
   components: {
+
+    Publication,
     HelloWorld
   }
 }
@@ -23,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,13 @@
 <template>
-  <div >
-    <li v-bind:key="user.iduser" v-for="user in users">
-      <input type="checkbox" :value="user" v-model="selected">- {{ user.name }} {{ user.surname }}<button v-on:click="deletebyiduser(user.iduser)">Х</button>
-    </li>
+  <div class="form">
+    <div class="form-users">
+        <li v-bind:key="user.iduser" v-for="user in users">
+      <input type="checkbox" :value="user" v-model="selected"><h5> {{ user.name }} {{ user.surname }}</h5><button v-on:click="deletebyiduser(user.iduser)">Удалить</button>
+       </li>
+    </div>
+
+      <div class="form-registration">
+        <h2>Регистрация пользователя</h2>
     {{name}}
     <label>Имя</label>
     <input v-model="name">
@@ -16,6 +21,7 @@
     <label>Пароль</label>
     <input v-model="password">
     <button v-on:click="addUser()">Добавить</button>
+      </div>
   </div>
 </template>
 
@@ -80,6 +86,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+template{
+
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -88,13 +97,64 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
+  display: flex;
+  flex-direction: row;
   margin: 0 10px;
+  width: 200px;
+  height: 30px;
+  border: 1px solid #42b983;
+  border-radius: 37px;
+  background: aqua;
+
 }
 a {
   color: #42b983;
 }
 button{
-padding: 5px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  border: 1px solid #42b983;
+  background: transparent;
+  border-radius: 37px;
+}
+ input {
+   border: 1px solid #42b983;
+   background: transparent;
+   border-radius: 37px;
+
+
+}
+h5{
+  display: flex;
+  justify-content: center;
+  font-size: 10px;
+  align-items: center;
+  width: 70px;
+}
+.form{
+  display: flex;
+  flex-direction: row;
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+  width: 100%;
+}
+.form-users{
+  width:100%;
+
+}
+.form-registration{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #42b983;
+  background: transparent;
+  border-radius: 37px;
+}
+h2{
+  font-size: 10px;
+  margin: 5px;
 }
 </style>
